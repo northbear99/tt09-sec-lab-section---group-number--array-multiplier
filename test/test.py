8dced395da3949f126b74a16f2ffd4c9dfca5cb1
northbear99 <74543213+northbear99@users.noreply.github.com>
2024-10-23 18:34:12 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -26,9 +26,8 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
+    dut.ui_in.value = 0b10..?
+    
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
@@ -38,3 +37,7 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+dut.ui_in.value = 0b10..?
+
+(need to change)
